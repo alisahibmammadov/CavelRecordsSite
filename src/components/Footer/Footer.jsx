4b1c082cab4a11f12028d331aa7c0eb8833f5627
alisahibmammadov <ali.yek.10@mail.ru>
2025-05-03 22:57:15 +0400

    Navbar and footer started dev

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,81 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <main className="flex items-center justify-center bg-[#212121] px-20 flex-col pt-16">
+      <div className="w-full flex items-start justify-around pb-5 flex-wrap flex-col sm:flex-row">
+        <div className="flex flex-col gap-2 py-2 ">
+          <h3 className="text-2xl text-white">Company</h3>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            About
+          </Link>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Blog
+          </Link>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Join Us
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2 py-2">
+          <h3 className="text-2xl text-white">Services</h3>
+          <Link
+            className=" hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Premium Account
+          </Link>
+          <Link
+            className=" hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Mobile App
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2 py-2">
+          <h3 className="text-2xl text-white">Support</h3>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Pricing Plan
+          </Link>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Documentation
+          </Link>
+          <Link
+            className="hover:text-blue-400 transition text-[16px] font-light text-white"
+            to="/"
+          >
+            Tutorial
+          </Link>
+        </div>
+      </div>
+      <div className="border-t-[1px] w-full border-white flex justify-between items-center py-5 flex-wrap gap-5">
+        <p className="text-white font-light text-[14px]">
+          Design with © 2025 right reserved
+        </p>
+        <div className="flex gap-2 items-center justify-center sm:justify-end  flex-1">
+          <i class="hover:text-blue-400 transition cursor-pointer text-white fa-brands fa-facebook-f"></i>
+          <i class="hover:text-blue-400 transition cursor-pointer text-white fa-brands fa-instagram"></i>
+          <i class="hover:text-blue-400 transition cursor-pointer text-white fa-brands fa-spotify"></i>
+          <i class="hover:text-blue-400 transition cursor-pointer text-white fa-brands fa-youtube"></i>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <main className="flex items-center justify-between bg-[#212121] opacity-90 fixed px-20 w-full py-4">
+      <div className="">
+        <h1 className="text-4xl text-[#f9f9f9]">
+          Cavel <i>Records</i>
+        </h1>
+      </div>
+      <div className="flex-none ">
+        <Link
+          className="hover:text-blue-400 transition px-2 text-white"
+          to="./home"
+        >
+          Home
+        </Link>
+        <Link
+          className="hover:text-blue-400 transition px-2 text-white"
+          to="./contact"
+        >
+          Contact
+        </Link>
+        <Link
+          className="hover:text-blue-400 transition px-2 text-white"
+          to="./producers"
+        >
+          Producers
+        </Link>
+        <Link
+          className="hover:text-blue-400 transition px-2 text-white"
+          to="./analytics"
+        >
+          Analytics
+        </Link>
+      </div>
+    </main>
+  );
 }
 
 export default Navbar;
